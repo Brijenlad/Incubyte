e1a9029d45e188b60dab64e89f33bb30d29723c8
Brijenlad <brijulad7@gmail.com>
2021-08-20 08:55:50 +0530

    created connection & fetch data from file.

---
+++ Incubyte.py
@@ -0,0 +1,9 @@
+import mysql.connector as cm
+#mydb = mysql.connector.connect(host="localhost",user="root",password="root")
+mydb = cm.connect(host="localhost",user="root",passwd="brijen#660")
+#file=open("sample.txt",'r')
+#for line in file:
+    #list=line.split('|')
+    #if(list[1]=='D'):
+        #print(list[2])
+#file.close()
